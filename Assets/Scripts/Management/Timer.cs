c45245c9a02fe8e1bfcc3ed985b813a6703411cc
Lauren D'Angelo <lkd180001@utdallas.edu>
2020-10-29 16:14:03 -0500

    Lose Screen & Timer
    Added a lose scene and a timer

---
+++ Assets/Scripts/Management/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _currentTime = null;
+    public float _timeLeft = 90.0f;
+
+    void Update()
+    {
+        _timeLeft -= Time.deltaTime;
+        if (_timeLeft <= 0)
+        {
+            GameOver();
+        }
+        DisplayTimeLeft();
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("LoseScreen");
+    }
+
+    void DisplayTimeLeft()
+    {
+        _currentTime.text = "Time Left: " + _timeLeft.ToString();
+    }
+}
