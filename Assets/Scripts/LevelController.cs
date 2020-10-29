10b86bee3a72175c1f0c3c0a1922386566ad9b4d
Lauren D'Angelo <lkd180001@utdallas.edu>
2020-10-28 22:16:57 -0500

    Level Controller
    Set up level controller, and pause stuff

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("UI")]
+    [SerializeField] GameObject _pauseMenu = null;
+
+
+    bool _isPaused = false;
+    int _currentScore = 0;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!_isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                UnpauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        _isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        _pauseMenu.SetActive(true);
+    }
+
+    public void UnpauseGame()
+    {
+        _isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        _pauseMenu.SetActive(false);
+    }
+
+    public void IncreaseScore(int increaseAmount)
+    {
+        _currentScore += increaseAmount;
     }
 }
